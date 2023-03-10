a23bd499e019a089df7ac9c4c6cf12d7be440050
Cary Carter <ccarterdev@gmail.com>
2023-03-10 12:36:06 -0600

    way better sh script thanks chatgpt

--- gitconf.sh
+++ gitconf.sh
@@ -1,3 +1,29 @@
-CMD="git config --$1 user.$2 '$3'"
-echo $CMD;
-$CMD;
+#!/bin/sh
+
+NAME="Cary Carter"
+EMAIL="$2"
+
+if [ -z "$EMAIL" ]
+then
+    echo "Please provide an email address"
+    exit 1
+fi
+
+if [ "$1" == "global" ]
+then
+    git config --global user.name "$NAME"
+    git config --global user.email "$EMAIL"
+    echo "Global git config updated with name/email: $NAME $EMAIL"
+elif [ "$1" == "local" ]
+then
+    git config user.name "$NAME"
+    git config user.email "$EMAIL"
+    echo "Local git config updated with name/email: $NAME $EMAIL"
+elif [ "$1" == "system" ]
+then
+    sudo git config --system user.name "$NAME"
+    sudo git config --system user.email "$EMAIL"
+    echo "System git config updated with name/email: $NAME $EMAIL"
+else
+    echo "Invalid option: use 'global', 'local', or 'system'"
+fi
