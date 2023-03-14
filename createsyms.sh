c1a64df99b4539e17afdaacb0f17db3926113203
Cary Carter <ccarterdev@gmail.com>
2023-03-14 10:11:14 -0500

    create syms

--- createsyms.sh
+++ createsyms.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
-rm -r ~/global/syms
-mkdir ~/global/syms
 
-ln -s ~/global/76.sh ~/global/syms/76
-ln -s ~/global/evar.sh ~/global/syms/evar
+rm ~/global/syms/*
+
 ln -s ~/global/ftemplate.py ~/global/syms/ftemplate
 ln -s ~/global/gcpports.sh ~/global/syms/gcpports
 ln -s ~/global/gcpssh.sh ~/global/syms/gcpssh
 ln -s ~/global/gitconf.sh ~/global/syms/gitconf
 ln -s ~/global/gpull.sh ~/global/syms/gpull
 ln -s ~/global/gpush.sh ~/global/syms/gpush
+ln -s ~/global/rcconf.py ~/global/syms/rcconf
