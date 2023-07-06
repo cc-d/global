4d89f49c2b2fa80fbf9280684949ebf3e458f6ff
Cary Carter <ccarterdev@gmail.com>
2023-07-06 12:40:40 -0500

    by gitacp alias

--- aliases.sh
+++ aliases.sh
@@ -7,3 +7,4 @@ alias ssh76='ssh work@192.168.254.100 -L 8008:localhost:8008 -L 3000:localhost:3
 alias gcpports='exec gcloud compute --project pict-app ssh --zone us-central1-a webapp-development-cary -- -NL 3000:localhost:3000 -NL 8000:localhost:8000'
 alias gcpssh='gcloud compute --project pict-app ssh --zone us-central1-a webapp-development-cary'
 
+alias gitacp='git add . && git commit -m "by gitacp alias" && git push'
