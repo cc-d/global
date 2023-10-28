7a1353c4bf3cd1b287ae4bd0f79ae31f365dd029
Cary Carter <ccarterdev@gmail.com>
2023-10-28 06:33:41 -0700

    globalshell/funcs.sh scripts/handwarmer.py test.sh...

--- test.sh
+++ test.sh
@@ -1,24 +1,7 @@
 #!/bin/sh
 
-list_dir() {
-    dir="$1"
-    indent="$2"
-    for entry in "$dir"/*; do
-        [ -e "$entry" ] || continue
-        entry_name=$(basename "$entry")
-        printf "%s%s\n" "$indent" "$entry_name"
-        if [ -d "$entry" ]; then
-            (
-                cd "$entry" || exit 1
-                list_dir "." "$indent  "
-                cd ..
-            )
-        fi
-    done
+listdir() {
+    dirtree=$(find "$1" -print | sed -e '1d' -e 's;[^/]*/;  ;g' -e 's;^  ;;')
+    echo "$dirtree" | grep -vE '.pyc$'
+
 }
-
-root_dir="."
-
-printf "%s\n" "$root_dir"  # Print the current directory
-list_dir "$root_dir" "  "  # Start with 2 spaces of initial indentation
-
