a981b46be47c8bbbf7c5836fefcd0fd8c44f9ee3
Cary Carter <ccarterdev@gmail.com>
2023-10-31 20:00:09 -0700

    broke funcs bc dupelicate func alia

--- globalshell/funcs.sh
+++ globalshell/funcs.sh
@@ -475,13 +475,6 @@ pasterun() {
 }
 
 
-pyenvinit(){
-  export PYENV_ROOT="$HOME/.pyenv"
-  command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
-  eval "$(pyenv init -)"
-}
-
-
 
 dirfiles() {
     _df_dir="$1"
