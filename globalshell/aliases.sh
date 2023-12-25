0c384d3274a525fef4f8755b58b5292f766bde27
Cary Carter <ccarterdev@gmail.com>
2023-12-24 16:13:22 -0800

    updated aliases

--- globalshell/aliases.sh
+++ globalshell/aliases.sh
@@ -4,11 +4,8 @@ alias gpull='git pull origin $BNAME'
 alias gpush='git push origin $BNAME'
 alias initgshell='. "$HOME/global/init-globalshell.sh"'
 
-alias ssh76='ssh work@192.168.254.100 -L 8008:localhost:8008 -L 3000:localhost:3000 -L 8000:localhost:8000'
 alias gcpports='exec gcloud compute --project pict-app ssh --zone us-central1-a webapp-development-cary -- -NL 3000:localhost:3000 -NL 8000:localhost:8000'
 alias gcpssh='gcloud compute --project pict-app ssh --zone us-central1-a webapp-development-cary'
-
-alias gitacp='git add . && git commit -m "by gitacp alias" && git push'
 
 alias pyenvinit='export PYENV_ROOT="$HOME/.pyenv" && \
 	command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH" \
@@ -22,3 +19,5 @@ alias py='python'
 alias shellsh='source shell.sh'
 
 alias git-ssh="git_ssh"
+
+alias dc='docker compose'
