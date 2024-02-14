4c99da314d1ee11a36dc5580c7b29c22529c3fff
Cary Carter <ccarterdev@gmail.com>
2024-02-13 16:23:12 -0800

    scripts/deps.sh ...

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
 [ "$(id -u)" -ne 0 ] && echo "Run as root" >&2 && exit 1
-apt-get update -yq && apt-get upgrade -yq
-apt-get install -yq software-properties-common git
+
+echo "Updating and Upgrading"
+apt-get update -yqq && apt-get upgrade -yqq
+
+echo "Downloading global repository and installing dependencies to do this"
+apt-get install -yqq software-properties-common git
 cd $HOME; git clone https://github.com/cc-d/global.git
-apt-get install -yq build-essential curl git libssl-dev libbz2-dev \
+
+echo "Installing rest of dependencies for pyenv/nvm/general use"
+apt-get install -yqq "build-essential curl git libssl-dev libbz2-dev \
 libreadline-dev libsqlite3-dev wget llvm libncurses5-dev libncursesw5-dev xz-utils \
 tk-dev libffi-dev liblzma-dev autoconf automake libtool nasm make pkg-config \
-libghc-zlib-dev libcurl4-gnutls-dev libexpat1-dev gettext unzip libz-dev libpq-dev openssl
+libghc-zlib-dev libcurl4-gnutls-dev libexpat1-dev gettext unzip libz-dev libpq-dev openssl"
+
+echo "Installing nvm and pyenv"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
 curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
 echo 'export NVM_DIR="$HOME/.nvm"' >> $HOME/.bashrc
