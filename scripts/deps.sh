1962824cd2dfdc810049ca894698c8bca1b02a04
Cary Carter <ccarterdev@gmail.com>
2024-02-13 16:12:19 -0800

    qq to q

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 [ "$(id -u)" -ne 0 ] && echo "Run as root" >&2 && exit 1
-apt-get update -yqq && apt-get upgrade -yqq
-apt-get install -yqq software-properties-common git
+apt-get update -yq && apt-get upgrade -yq
+apt-get install -yq software-properties-common git
 cd $HOME; git clone https://github.com/cc-d/global.git
-apt-get install -yqq build-essential curl git libssl-dev libbz2-dev \
+apt-get install -yq build-essential curl git libssl-dev libbz2-dev \
 libreadline-dev libsqlite3-dev wget llvm libncurses5-dev libncursesw5-dev xz-utils \
 tk-dev libffi-dev liblzma-dev autoconf automake libtool nasm make pkg-config \
 libghc-zlib-dev libcurl4-gnutls-dev libexpat1-dev gettext unzip libz-dev libpq-dev openssl
@@ -15,3 +15,4 @@ echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> $HOME/.bashrc
 echo 'eval "$(pyenv init --path)"' >> $HOME/.bashrc
 git clone https://github.com/cc-d/global.git $HOME/global
 echo '. ~/global/init-globalshell.sh' >> $HOME/.bashrc
+
