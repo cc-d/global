ff49c81ff9f0805480f4f6110dc4b2c2523e8707
Cary Carter <ccarterdev@gmail.com>
2024-02-21 06:47:03 -0800

    before work work

--- globalshell/funcs/shell.sh
+++ globalshell/funcs/shell.sh
@@ -2,14 +2,18 @@
 
 history() {
     if [ -z "$GLOBAL_SHELL_HISTORY" ]; then
+        echo "GLOBAL_SHELL_HISTORY is not set, using default"
         command history "$@"
     else
         if [ -f "$HOME/.zsh_history" ]; then
-            cat "$HOME/.zsh_history" | uniq
+            _GC_HISTORY_FILE="$HOME/.zsh_history"
         elif [ -f "$HOME/.bash_history" ]; then
-            cat "$HOME/.bash_history" | uniq
+            _GC_HISTORY_FILE="$HOME/.bash_history"
         else
             echo "No history file found"
+            return 1
         fi
     fi
+
+    awk '{$1=$1}; !x[$0]++' "$_GC_HISTORY_FILE" | nl -w 1 -s " " -ba
 }
