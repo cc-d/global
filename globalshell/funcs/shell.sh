f7044b0b2b1d88efd645bdc208b95274e5f802da
Cary Carter <ccarterdev@gmail.com>
2024-03-07 16:51:18 -0800

    [2 FILE(s)] cron/ghistory.py globalshell/funcs/shell.sh

--- globalshell/funcs/shell.sh
+++ globalshell/funcs/shell.sh
@@ -5,12 +5,6 @@ history() {
         echo "GLOBAL_SHELL_HISTORY is not set, using default"
         command history "$@"
     else
-        python3 "$HOME/global/cron/gc_history.py"
-        _GC_HISTORY_FILE="$HOME/.global/shell_history"
-        for _GC_HIST_LINE in \
-            `awk '{$1=$1}; !x[$0]++' "$_GC_HISTORY_FILE" | nl -w 1 -s " " -ba`;
-        do
-            echo "$_GC_HIST_LINE"
-        done
+        python3 "$HOME/global/cron/ghistory.py" print
     fi
 }
