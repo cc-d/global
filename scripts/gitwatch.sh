3c6f1e2f30c5840b2533f48a471989c8ff332b8b
Cary Carter <ccarterdev@gmail.com>
2024-04-29 15:27:56 -0700

    gitwatcher

--- scripts/gitwatch.sh
+++ scripts/gitwatch.sh
@@ -3,17 +3,17 @@
 # Print usage information
 usage() {
     echo ""
-    echo "Usage: $0 [options] <path_to_git_repo> [interval_in_seconds]"
-    echo "Options: \n"
+    echo "Usage: $0 [options] [path_to_git_repo] [interval_in_seconds]"
+    echo "Options:"
     echo "  -h, --help           Show this help message"
-    echo "  -n, --no-pull            Run pre-pull and post-pull actions without pulling"
+    echo "  -n, --no-pull        Run pre-pull and post-pull actions without pulling"
     echo ""
 }
 
 # Initialize variables
-_NO_PULL=0
-_REPO_PATH=""
-_INTERVAL=""
+GITWATCH_NO_PULL=0
+GITWATCH_REPO_PATH=""
+GITWATCH_INTERVAL=""
 
 # Parse command line arguments
 while [ "$#" -gt 0 ]; do
@@ -23,15 +23,15 @@ while [ "$#" -gt 0 ]; do
             exit 0
             ;;
         -n|--no-pull)
-            _NO_PULL=1
+            GITWATCH_NO_PULL=1
             shift
             ;;
         *)
-            if [ -d "$1" ]; then
-                _REPO_PATH="$1"
+            if [ -d "$1" ] && [ -z "$GITWATCH_REPO_PATH" ]; then
+                GITWATCH_REPO_PATH="$1"
                 shift
-            elif echo "$1" | grep -q '^[0-9]+$'; then
-                _INTERVAL="$1"
+            elif echo "$1" | grep -qE '^[0-9]+$' && [ -z "$GITWATCH_INTERVAL" ]; then
+                GITWATCH_INTERVAL="$1"
                 shift
             else
                 echo "Error: Invalid argument $1"
@@ -42,31 +42,36 @@ while [ "$#" -gt 0 ]; do
     esac
 done
 
-# Set default repository path to the script's directory if not provided
-if [ -z "$_REPO_PATH" ]; then
-    _REPO_PATH="$(cd "$(dirname "$0")" && pwd)"
+# Set default repository path if not provided
+if [ -z "$GITWATCH_REPO_PATH" ]; then
+    GITWATCH_REPO_PATH=$(pwd)
+    while [ "$GITWATCH_REPO_PATH" != "/" ] && [ ! -d "$GITWATCH_REPO_PATH/.git" ]; do
+        GITWATCH_REPO_PATH=$(dirname "$GITWATCH_REPO_PATH")
+    done
+    if [ ! -d "$GITWATCH_REPO_PATH/.git" ]; then
+        echo "Error: No .git directory found in any parent directory."
+        exit 1
+    fi
 fi
 
-REPO_NAME=$(basename "$_REPO_PATH")
+REPO_NAME=$(basename "$GITWATCH_REPO_PATH")
 PROCESS_NAME="GitWatch_$REPO_NAME"
 
-# Ensure repository path is provided
-if [ -z "$_REPO_PATH" ]; then
-    echo "Error: Repository path is required."
-    usage
-    exit 1
-fi
+# Kill existing instances of the GitWatch screen session
+kill_existing_sessions() {
+    # Wipe out dead screen sessions first
+    screen -wipe
 
-# Function to handle termination of existing screen sessions
-kill_existing_session() {
-    if screen -ls | grep -q "$PROCESS_NAME"; then
-        echo "An existing process was found for $PROCESS_NAME. Killing..."
-        screen -S "$PROCESS_NAME" -X quit
-        echo "Process killed."
-    fi
+    # Now fetch all existing session IDs and names, filter for our process, and terminate them
+    screen -ls | grep "$PROCESS_NAME" | awk '{print $1}' | while IFS= read -r session_info
+    do
+        session_id=$(echo "$session_info" | awk -F '.' '{print $1}')
+        echo "Terminating existing GitWatch session: $session_id"
+        screen -S "$session_id" -X quit
+    done
 }
 
-# Define default pre-pull and post-pull actions
+# Define functions for pre-pull and post-pull actions
 pre_pull_action() {
     echo "Pre-pull action executed."
 }
@@ -77,36 +82,37 @@ post_pull_action() {
 
 # Function to execute Git operations
 git_operations() {
-    cd "$_REPO_PATH" || exit 1
+    cd "$GITWATCH_REPO_PATH" || exit 1
     git fetch
     LOCAL_COMMIT=$(git rev-parse HEAD)
     REMOTE_COMMIT=$(git rev-parse "@{u}")
 
     if [ "$LOCAL_COMMIT" != "$REMOTE_COMMIT" ]; then
-        pre_pull_action || return 1
-        if [ -z "$_NO_PULL" ]; then
+        pre_pull_action
+        if [ "$GITWATCH_NO_PULL" -eq 0 ]; then
             echo "Changes detected. Pulling changes..."
-            git pull --quiet || return 1
+            git pull
         else
             echo "Pulling changes skipped."
         fi
-        post_pull_action || return 1
-        echo "Operations completed."
+        post_pull_action
     else
         echo "No changes detected."
     fi
+
+    if [ -n "$GITWATCH_REPEAT" ]; then
+        sleep "$GITWATCH_INTERVAL"
+        git_operations
+    fi
 }
 
-# Check and manage screen session for interval-based execution
-if [ -n "$_INTERVAL" ]; then
-    kill_existing_session
-    # Start the script in a new screen session with necessary environment variables
-    screen -dmS "$PROCESS_NAME" \
-        env GITWATCH_REPO_PATH="$_REPO_PATH" GITWATCH_INTERVAL="$_INTERVAL" \
-        sh -c "$0 --no-pull \"$_REPO_PATH\" \"$_INTERVAL\""
+# Manage screen session
+if [ -n "$GITWATCH_INTERVAL" ]; then
+    kill_existing_sessions
+    GITWATCH_REPEAT=1
+    screen -dmS "$PROCESS_NAME" sh -c "env GITWATCH_REPO_PATH=\"$GITWATCH_REPO_PATH\" GITWATCH_INTERVAL=\"$GITWATCH_INTERVAL\" GITWATCH_NO_PULL=\"$GITWATCH_NO_PULL\" GITWATCH_REPEAT=\"$GITWATCH_REPEAT\" $0"
     echo "Spawned a new background screen session named $PROCESS_NAME"
     exit 0
 fi
 
-# Execute Git operations directly if no interval is provided
 git_operations
