51450e7bc0f736c5a07a1b8f388e2c81eb4216b6
Cary Carter <ccarterdev@gmail.com>
2024-05-20 18:35:36 -0700

    1 FILE(s) | scripts/multifind.py

--- scripts/multifind.py
+++ scripts/multifind.py
@@ -6,11 +6,41 @@ import threading
 import re
 from time import sleep
 from glob import glob
-from typing import List
+from typing import List, Optional as Opt, Union as U
+from logfunc import logf
+
+
+def _search_line(line: str, args: argparse.Namespace) -> U[str, None]:
+    """Handles single line in file returning the line if conditions are met."""
+    matches = {}
+
+    for search_str in args.search:
+        if args.regex:
+            match = re.search(
+                search_str, line, re.IGNORECASE if args.ignore_case else 0
+            )
+            if match:
+                if args.search_mode == 'or':
+                    return match.group(0) if args.partial else line
+                matches[search_str] = match.group(0) if args.partial else line
+        else:
+            search_line = line.lower() if args.ignore_case else line
+            search_str = search_str.lower() if args.ignore_case else search_str
+            if search_str in search_line:
+                if args.search_mode == 'or':
+                    return search_str if args.partial else line
+                matches[search_str] = line
+
+    if args.search_mode == 'and' and len(matches) == len(args.search):
+        return line
 
 
 def find_in_file_segment(
-    filename, start, end, search_strs, regex, partial, results
+    filename: str,
+    start: int,
+    end: int,
+    args: argparse.Namespace,
+    results: dict,
 ):
     """Find the search string in a file segment from start to end."""
 
@@ -23,36 +53,29 @@ def find_in_file_segment(
             line = file.readline()
             if file.tell() > end:
                 break
-            for search_str in search_strs:
-                if regex:
-                    match = re.search(search_str, line)
-                    if match:
-                        _results.append(match.group(0) if partial else line)
-                        print(_results[-1])
-                else:
-                    if search_str in line:
-                        _results.append(search_str if partial else line)
-                        print(_results[-1])
-
+            sline = _search_line(line, args)
+            if sline:
+                print(sline)
+                _results.append(sline)
     results[filename] = _results
 
 
 def thread_file_processing(
-    filename, search_strs, regex, partial, num_threads, results
+    filename: str, args: argparse.Namespace, results: dict
 ):
     """Process a file using multiple threads."""
 
     filesize = os.path.getsize(filename)
-    segment_size = filesize // num_threads
+    segment_size = filesize // args.num_threads
     threads = []
 
-    for i in range(num_threads):
+    for i in range(args.num_threads):
         start = i * segment_size
-        end = start + segment_size if i < num_threads - 1 else filesize
+        end = start + segment_size if i < args.num_threads - 1 else filesize
 
         thread = threading.Thread(
             target=find_in_file_segment,
-            args=(filename, start, end, search_strs, regex, partial, results),
+            args=(filename, start, end, args, results),
         )
         threads.append(thread)
         thread.start()
@@ -61,33 +84,35 @@ def thread_file_processing(
         thread.join()
 
 
-def process_files(files, search_strs, regex, partial, num_workers):
+def process_files(files, args):  # search, regex, partial, num_workers):
     """Setup file processing with appropriate concurrency."""
+    num_workers = args.cpu
     with M.Pool(num_workers) as pool, M.Manager() as manager:
         results = manager.dict()
+        procs = []
         for filename in files:
-            pool.apply_async(
-                thread_file_processing,
-                (filename, search_strs, regex, partial, num_workers, results),
+            procs.append(
+                pool.apply_async(
+                    thread_file_processing, (filename, args, results)
+                )
             )
-        while len(results) < len(files):
+        while not all(proc.ready() for proc in procs):
             sleep(0.1)
 
 
 def main(args):
-    log_files = glob(args.file_pattern)
-
-    process_files(log_files, args.search, args.regex, args.partial, args.cpu)
+    log_files = glob(args.files)
+    process_files(log_files, args)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Log file analysis tool.')
     parser.add_argument(
         '-f',
-        '--file_pattern',
+        '--files',
         type=str,
         default='*.log',
-        help='Pattern to match log files',
+        help='Pattern to match log files (glob)',
     )
     parser.add_argument(
         '-c',
@@ -100,8 +125,9 @@ if __name__ == '__main__':
         '-t',
         '--threads',
         type=int,
-        default=os.cpu_count() * 4,
-        help='Number of threads per file, default is 4x per core',
+        default=4,
+        help='Number of threads per file, default is 4',
+        dest='num_threads',
     )
     parser.add_argument(
         '-r', '--regex', action='store_true', help='Use regex for searching'
@@ -113,7 +139,31 @@ if __name__ == '__main__':
         help='Display only matches, not full lines',
     )
     parser.add_argument(
-        'search', nargs='+', help='Search strings to look for in the files'
+        '-m',
+        '--search-mode',
+        choices=['and', 'or'],
+        default='or',
+        help='Search mode to use, default is AND',
+        type=str,
+        dest='search_mode',
     )
+    parser.add_argument(
+        '-i',
+        '--ignore-case',
+        action='store_true',
+        help='Ignore case when searching',
+    )
+    parser.add_argument(
+        'search',
+        nargs='*',
+        type=str,
+        help='Search string(s) to find in log files',
+    )
+    import sys
+    import shlex
+
+    ex = shlex.split(''.join(sys.argv[1:]))
     args = parser.parse_args()
+    print(args)
+    args.search_mode = args.search_mode.lower()
     main(args)
