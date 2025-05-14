d526b6594b1942d7064b56ad99ad77367142647d
Cary Carter <ccarterdev@gmail.com>
2025-05-14 15:48:13 -0500

    minor aesthetic changes

--- scripts/tmpdiff.sh
+++ scripts/tmpdiff.sh
@@ -3,11 +3,15 @@
 f1=`mktemp`
 f2=`mktemp`
 
+# trap "rm -rf $f1 $f2" EXIT INT TERM
+
+echo "$f1\n$f2\n"
+
 exec > $f1
 $@
 exec > /dev/tty
 
-read -p "Continue>"
+read -p "Hit Enter to Continue: "
 
 exec > $f2
 $@
